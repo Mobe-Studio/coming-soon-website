a10d2f0cf768568d168633ec8b2e99f12a434f15
Giorgio Bertolotti <giorgiobertol@gmail.com>
2023-11-17 12:52:35 +0100

    :sparkles: Configure the project for dist build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-between p-6 lg:p-20">
       <div></div>
-      <div className="flex flex-col items-center gap-6 md:gap-12 lg:gap-16">
+      <div className="flex max-w-[300px] flex-col items-center gap-6 md:max-w-[500px] md:gap-12 lg:max-w-[700px] lg:gap-16">
         <h1>mobe studio</h1>
         <p className="text-center text-2xl font-light md:text-5xl lg:text-6xl">
           Digital Experience studio based in Bergamo (Italy)
