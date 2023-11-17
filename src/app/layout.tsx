701e25648fc2be19a737b76b8c27659696359dc6
Giorgio Bertolotti <giorgiobertol@gmail.com>
2023-11-17 12:38:16 +0100

    :sparkles: Add first version of website

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import './globals.css';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="it">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="keywords" content="Design, Software, Web, Website, Studio, Agency, Responsive" />
+        <meta name="author" content="Mobe Studio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Coming soon - Mobe Studio</title>
+        <meta property="og:title" content="Mobe Studio" />
+        <meta property="twitter:title" content="Mobe Studio" />
+        <meta property="og:locale" content="en_US" />
+        <meta name="description" content="Digital Experience studio based in Bergamo (Italy)" />
+        <meta property="og:description" content="Digital Experience studio based in Bergamo (Italy)" />
+        <meta property="twitter:description" content="Digital Experience studio based in Bergamo (Italy)" />
+        <link rel="canonical" href="https://mobestudio.com/" />
+        <meta property="og:url" content="https://mobestudio.com/" />
+        <meta property="og:site_name" content="Mobe Studio" />
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:image" content="https://mobestudio.com/images/logo.png" />
+        <meta prefix="og: http://ogp.me/ns#" name="og:title" property="og:title" content="Mobe Studio" />
+        <meta prefix="og: http://ogp.me/ns#" name="og:type" property="og:type" content="website" />
+        <meta
+          prefix="og: http://ogp.me/ns#"
+          name="og:image"
+          property="og:image"
+          content="https://mobestudio.com/images/logo.png"
+        />
+        <meta prefix="og: http://ogp.me/ns#" name="og:url" property="og:url" content="https://mobestudio.com/" />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen flex-col items-center justify-between p-6 lg:p-20">
+      <div></div>
+      <div className="flex flex-col items-center gap-6 md:gap-12 lg:gap-16">
+        <h1>mobe studio</h1>
+        <p className="text-center text-2xl font-light md:text-5xl lg:text-6xl">
+          Digital Experience studio based in Bergamo (Italy)
+        </p>
+      </div>
+      <p className="text-lg font-normal uppercase md:text-3xl lg:text-4xl">Online soon</p>
+    </main>
+  );
+}
