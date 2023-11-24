9a791c14ff1a6500f8e4cbd51fefec27f7c005e5
Giorgio Bertolotti <giorgiobertol@gmail.com>
2023-11-24 10:48:20 +0100

    :lipstick: Small changes to chips style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ const SLEEP_BEFORE_CHIPS_SECS =
   LOGO_ANIMATION_DURATION_SECS + DESCRIPTION_ANIMATION_DURATION_SECS + ONLINE_SOON_ANIMATION_DURATION_SECS + 1;
 const SLEEP_BEFORE_TOP_BOUND_SECS = SLEEP_BEFORE_CHIPS_SECS + 2;
 const CHIP_WIDTH = 180;
-const CHIP_HEIGHT = 48;
+const CHIP_HEIGHT = 60;
 const CHIPS = [
   'Code',
   'Development',
@@ -158,26 +158,25 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex h-[100svh] flex-col items-center justify-between p-6 lg:p-8">
-      <div></div>
+    <main className="flex h-[100svh] flex-col items-center p-6 lg:p-8">
       <div className="flex max-w-[300px] flex-col items-center gap-0 md:max-w-[500px] lg:max-w-[600px] lg:gap-8">
         <div ref={logoRef} className="w-full">
           <Logo alt="Mobe Studio" title="Mobe Studio" className="w-full" />
         </div>
-        <p ref={descriptionRef} className="text-center text-2xl font-light md:text-4xl md:leading-[120%]">
+        <p ref={descriptionRef} className="mb-8 text-center text-2xl font-light md:text-4xl md:leading-[120%] lg:mb-6">
           Digital Experience studio
           <br />
           based in Bergamo (Italy)
         </p>
+        <p ref={onlineSoonRef} className="text-base font-normal uppercase lg:text-lg">
+          Online soon
+        </p>
       </div>
-      <p ref={onlineSoonRef} className="text-base font-normal uppercase lg:text-lg">
-        Online soon
-      </p>
       {CHIPS.map((chip, i) => (
         <div
           ref={ref => ref && (chipRefs.current[i] = ref)}
           key={i}
-          className="absolute flex select-none items-center justify-center rounded-full bg-white text-lg font-normal uppercase text-black"
+          className="absolute flex select-none items-center justify-center rounded-full bg-white text-lg font-semibold text-black"
           style={{
             width: CHIP_WIDTH,
             height: CHIP_HEIGHT,
